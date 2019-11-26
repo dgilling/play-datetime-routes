abc5a7b881b989a2b266160dd6096dbdcbd7520f
Devendra <devendra@Devendras-MacBook-Pro.local>
2019-11-25 18:51:06 -0800

    Add time zone support and rest from to interval  boundary

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 name := "play-datetime-routes"
 organization := "com.github.dgilling"
-version := "1.0.0"
+version := "1.0.1"
 
 publishMavenStyle := true
 isSnapshot := true
--- src/main/scala/com/github/dgilling/datetimeroutes/JodaDateTimeRoutes.scala
+++ src/main/scala/com/github/dgilling/datetimeroutes/JodaDateTimeRoutes.scala
@@ -2,24 +2,40 @@ package com.github.dgilling.datetimeroutes
 
 import org.joda.time.format.DateTimeFormat
 import play.api.mvc.{PathBindable, QueryStringBindable}
-import org.joda.time.{DateTimeZone, DateTime}
+import org.joda.time.{DateTime, DateTimeZone}
+
 import scala.util.matching.Regex._
 
 /**
   * Created by derric on 5/12/16.
   */
+
+class DateTimeParsing[A](parse: (String, String, DateTimeZone) => A, serialize: A => String, error: (String, Exception) => String)
+  extends QueryStringBindable[A] {
+
+  def bind(key: String, params: Map[String, Seq[String]]) = params.get(key).flatMap(_.headOption).map { dateString =>
+    try {
+      val timeZone = DateTimeZone.forID(params.get("time_zone").flatMap(_.headOption).getOrElse("UTC"))
+      Right(parse(key, dateString, timeZone))
+    } catch {
+      case e: Exception => Left(error(key, e))
+    }
+  }
+  def unbind(key: String, value: A) = key + "=" + serialize(value)
+}
+
 trait JodaDateTimeRoutes { self: JodaFormat =>
 
   val format: String
 
-  implicit object queryStringPeriodBinder extends QueryStringBindable.Parsing[DateTime](
-    dateString =>  JodaPeriodConverter.parsePeriod(dateString, format),
+  implicit object queryStringDateRangeBinder extends DateTimeParsing[DateTime](
+    (key,  dateString, timeZone) =>  if (key.toLowerCase == "from") JodaPeriodConverter.parseFromPeriod(dateString, format, timeZone) else JodaPeriodConverter.parsePeriod(dateString, format, timeZone),
     _.toString(format),
-    (key: String, e: Exception) => "Cannot parse parameter %s as org.joda.time.DateTime: %s".format(key, e.getMessage)
+    (key: String, e: Exception) => "Cannot parse parameter %s as com.github.dgilling.datetimeroutes.DateRange: %s".format(key, e.getMessage)
   )
 
   implicit object pathPeriodBinder extends PathBindable.Parsing[DateTime](
-    dateString =>  JodaPeriodConverter.parsePeriod(dateString, format),
+    dateString =>  JodaPeriodConverter.parsePeriod(dateString,  format, DateTimeZone.UTC),
     _.toString(format),
     (key: String, e: Exception) => "Cannot parse parameter %s as org.joda.time.DateTime: %s".format(key, e.getMessage)
   )
@@ -37,16 +53,43 @@ object JodaPeriodConverter {
   val WeeksRegex = """([+-]?)(\d+)[Ww]""".r
   val MonthsRegex = """([+-]?)(\d+)[M]""".r
 
-  def parsePeriod(dateTime: String, format: String): DateTime = {
-    dateTime match {
-      case "now" => DateTime.now(DateTimeZone.UTC)
-      case SecondsRegex(sign: String, value: String) => DateTime.now.plusSeconds((sign + value).toInt)
-      case MinutesRegex(sign, value) => DateTime.now.plusMinutes((sign + value).toInt)
-      case HoursRegex(sign, value) => DateTime.now.plusHours((sign + value).toInt)
-      case DaysRegex(sign, value) => DateTime.now.plusDays((sign + value).toInt)
-      case WeeksRegex(sign, value) => DateTime.now.plusWeeks((sign + value).toInt)
-      case MonthsRegex(sign, value) => DateTime.now.plusMonths((sign + value).toInt)
-      case _ => DateTimeFormat.forPattern(format).parseDateTime(dateTime)
+  trait Interval
+  object Daily extends Interval
+  object Weekly extends Interval
+  object Monthly extends Interval
+
+  def parseFromPeriod(fromString: String, format: String, timeZone: DateTimeZone): DateTime = {
+    // if timerange is relative, then from will be reset to start of the interval
+    val fromResetFcn = Option((interval: Interval, dateTime: DateTime) => {
+      Option(interval).collect {
+        case Daily => (dateTime: DateTime) => dateTime.withMillisOfDay(0)
+        case Weekly => (dateTime: DateTime) => dateTime.withDayOfWeek(1).withMillisOfDay(0)
+        case Monthly => (dateTime: DateTime) => dateTime.withDayOfMonth(1).withMillisOfDay(0)
+      }.map(_(dateTime)).getOrElse(dateTime)
+    })
+
+    parsePeriod(fromString, format, timeZone, fromResetFcn)
+  }
+
+  def parsePeriod(dateString: String, format: String, dateTimeZone: DateTimeZone,
+                    intervalResetBoundaryFcn: Option[(Interval, DateTime) => DateTime] = None): DateTime = {
+    val dateTime = dateString match {
+      case "now" => DateTime.now(dateTimeZone)
+      case SecondsRegex(sign: String, value: String) => DateTime.now(dateTimeZone).plusSeconds((sign + value).toInt)
+      case MinutesRegex(sign, value) => DateTime.now(dateTimeZone).plusMinutes((sign + value).toInt)
+      case HoursRegex(sign, value) => DateTime.now(dateTimeZone).plusHours((sign + value).toInt)
+      case DaysRegex(sign, value)  =>
+        val time = DateTime.now(dateTimeZone).plusDays((sign + value).toInt)
+        intervalResetBoundaryFcn.map(_(Daily, time)).getOrElse(time)
+      case WeeksRegex(sign, value) =>
+        val time = DateTime.now(dateTimeZone).plusWeeks((sign + value).toInt)
+        intervalResetBoundaryFcn.map(_(Weekly, time)).getOrElse(time)
+      case MonthsRegex(sign, value) =>
+        val time = DateTime.now(dateTimeZone).plusMonths((sign + value).toInt)
+        intervalResetBoundaryFcn.map(_(Monthly, time)).getOrElse(time)
+      case _ => DateTimeFormat.forPattern(format).parseDateTime(dateString)
     }
+    // backend always process dateTime in UTC, convert to UTC time
+    dateTime.toDateTime(DateTimeZone.UTC)
   }
 }
